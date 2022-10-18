5482a9c1defcef626239ee8b3d7f401aa555194c
Selig Lau <enterpriserpa-selig@gmail.com>
2022-10-18 16:05:56 +0100

    POC done

--- test.py
+++ test.py
@@ -1,5 +1,13 @@
 import whisper
+import torch
+import datetime
 
-model = whisper.load_model("base")
-result = model.transcribe("news.mp3")
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+model = whisper.load_model("base.en", device = "cpu")
+result = model.transcribe("news_3mins.mp3")
+strAppendTitle = str("\n===== Processed at "+ datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S") + " =====\n")
+print(strAppendTitle)
 print(result["text"])
+with open("output.txt", "a") as outputFile:
+    outputFile.write(str(strAppendTitle))
+    outputFile.write(str(result["text"]))

35ba32a098628cbec0351061eaedb1d54c9b802a
Selig Test ID <selig.lau@enterpriserpa.co.uk>
2022-11-02 09:37:07 +0000

    Update ERPA-SpeechToText.py

--- ERPA-SpeechToText.py
+++ ERPA-SpeechToText.py
@@ -2,9 +2,8 @@ import whisper
 import torch
 
 def main(fileName):
-
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     model = whisper.load_model("base.en", device = "cpu")
     result = model.transcribe(fileName)
     print(result["text"])
-    return result["text"];
+    return result["text"]

c7ba95129224eac93a1363484305e55d81f7041b
Selig Lau <selig.lau@enterpriserpa.co.uk>
2023-02-01 15:47:07 +0000

    added convertion code

--- amrToText.py
+++ amrToText.py
@@ -1,10 +1,16 @@
 import whisper
 import torch
 import datetime
+from pydub import AudioSegment
 
+input_file = input("Enter the input file name (including the extension): ")
+output_file = "output.mp3"
+
+sound = AudioSegment.from_file(input_file, format="amr")
+sound.export(output_file, format="mp3")
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 model = whisper.load_model("base.en", device = "cpu")
-result = model.transcribe("news_3mins.mp3")
+result = model.transcribe(output_file)
 strAppendTitle = str("\n===== Processed at "+ datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S") + " =====\n")
 print(strAppendTitle)
 print(result["text"])
